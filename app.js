a58d72cc0622ab3be197a25446aafca77d7a65f5
Hazri <hazrijordan@gmail.com>
2019-12-15 06:59:30 +0800

    get(api/v/tours) endpoint

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello from server side!', app: 'Mytours' });
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours
+    }
+  });
 });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint..😼');
-});
+// =================================================================================
+// app.get('/', (req, res) => {
+//   res.status(200).json({ message: 'Hello from server side!', app: 'Mytours' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint..😼');
+// });
+// =================================================================================
 
 // PORT
 const port = 3000;
