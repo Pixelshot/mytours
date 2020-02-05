0c2f498218a2149c2552597bdaba9eea267a75f9
Hazri <hazrijordan@gmail.com>
2020-02-05 10:32:04 +0800

    Sorting done

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -14,9 +14,16 @@ exports.getAllTours = async (req, res) => {
     // Advanced Filtering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-    console.log(JSON.parse(queryStr));
 
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
 
     // EXECUTE QUERY
     const tours = await query;
